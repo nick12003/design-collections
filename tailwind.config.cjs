2fb5f92ade318647468054b74683e204b701e4da
nick12003 <34929382+nick12003@users.noreply.github.com>
2023-02-24 00:08:40 +0800

    refactor: AnimatedCountdown移除tailwind寫法

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -41,26 +41,6 @@ module.exports = {
           '0%': { left: '0%' },
           '100%': { left: '-100%' },
         },
-        AnimatedCountdown_Hide: {
-          '0%': { transform: 'translate(-50%, -50%) scale(1)' },
-          '100%': { transform: 'translate(-50%, -50%) scale(0)' },
-        },
-        AnimatedCountdown_Show: {
-          '0%': { transform: 'translate(-50%, -50%) scale(0)' },
-          '35%': { transform: 'translate(-50%, -50%) scale(1.4)' },
-          '100%': { transform: 'translate(-50%, -50%) scale(1)' },
-        },
-        AnimatedCountdown_goIn: {
-          '0%': { transform: 'translate(-50%, -50%) rotate(120deg)' },
-          '30%': { transform: 'translate(-50%, -50%) rotate(-20deg)' },
-          '60%': { transform: 'translate(-50%, -50%) rotate(10deg)' },
-          '100%': { transform: 'translate(-50%, -50%) rotate(0deg)' },
-        },
-        AnimatedCountdown_goOut: {
-          '0%': { transform: 'translate(-50%, -50%) rotate(0deg)' },
-          '60%': { transform: 'translate(-50%, -50%) rotate(20deg)' },
-          '100%': { transform: 'translate(-50%, -50%) rotate(-120deg)' },
-        },
         ButtonRippleEffect_scale: {
           '0%': { transform: 'translate(-50%, -50%) scale(0)', opacity: 1 },
           '100%': { transform: 'translate(-50%, -50%) scale(3)', opacity: 0 },
@@ -69,10 +49,6 @@ module.exports = {
       animation: {
         ShowMenu: 'ShowMenu 0.5s ease-in-out forwards',
         HideMenu: 'HideMenu 0.5s ease-in-out forwards',
-        AnimatedCountdown_Hide: 'AnimatedCountdown_Hide 0.2s ease-out',
-        AnimatedCountdown_Show: 'AnimatedCountdown_Show 0.2s ease-out',
-        AnimatedCountdown_goIn: 'AnimatedCountdown_goIn 0.5s ease-out',
-        AnimatedCountdown_goOut: 'AnimatedCountdown_goOut 0.5s ease-out',
         ButtonRippleEffect_scale: 'ButtonRippleEffect_scale 0.5s ease-out',
       },
     },
