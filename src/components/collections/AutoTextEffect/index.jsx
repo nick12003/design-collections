0de806980835e4b8b889bdf6e63089c4086edfe6
nick12003 <34929382+nick12003@users.noreply.github.com>
2023-02-25 15:09:49 +0800

    refactor: AutoTextEffect移除tailwind寫法

--- src/components/collections/AutoTextEffect/index.jsx
+++ src/components/collections/AutoTextEffect/index.jsx
@@ -1,5 +1,7 @@
 import { useState, useRef, useEffect } from 'react';
 
+import styles from './style.module.scss';
+
 const AutoTextEffect = () => {
   const txt = '看好了世界，台灣只示範一次，一個禮拜停電兩次!';
   const timer = useRef();
@@ -11,7 +13,7 @@ const AutoTextEffect = () => {
       if (index + 1 > txt.length) {
         setIndex(1);
       } else {
-        setIndex(index + 1);
+        setIndex((preIndex) => preIndex + 1);
       }
     }, 1000 / speed);
     return () => {
@@ -20,13 +22,12 @@ const AutoTextEffect = () => {
   }, [index, speed]);
 
   return (
-    <div className="h-full bg-[darksalmon] flex flex-col items-center justify-center overflow-hidden text-lg">
-      <div className="text-3xl font-black">{txt.slice(0, index)}</div>
-      <div className="absolute bottom-[20px] bg-black/10 px-[20px] py-[10px]">
+    <div className={styles.AutoTextEffect}>
+      <h1>{txt.slice(0, index)}</h1>
+      <div className={styles.control}>
         <label htmlFor="speed">speed:</label>
         <input
           id="speed"
-          className="bg-[darksalmon] w-[50px] p-[5px] text-center"
           type="number"
           name="speed"
           value={speed}
