9bed2c10ac49aaf02909d175ad964d5d743349a9
nick12003 <34929382+nick12003@users.noreply.github.com>
2023-02-25 17:38:56 +0800

    refactor: CustomRangeSlider移除tailwind寫法

--- src/components/collections/CustomRangeSlider/index.jsx
+++ src/components/collections/CustomRangeSlider/index.jsx
@@ -1,18 +1,12 @@
 import { useState } from 'react';
-import classNames from 'classnames';
-
 import styles from './style.module.scss';
 
 const CustomRangeSlider = () => {
   const [value, setValue] = useState(50);
   return (
-    <div className="h-full flex flex-col items-center justify-center relative">
-      <div className="relative">
+    <div className={styles.CustomRangeSlider}>
+      <div className={styles['range-container']}>
         <input
-          className={classNames(
-            'w-[300px] mx-0 my-[18px] appearance-none focus:outline-none ',
-            styles.customerSlider
-          )}
           type="range"
           value={value}
           min="0"
@@ -21,11 +15,7 @@ const CustomRangeSlider = () => {
             setValue(e.target.value);
           }}
         />
-        <label
-          htmlFor="range"
-          className=" bg-white absolute top-[-20px] left-[110px] w-[80px] py-0 px-[5px] text-center rounded-[4px] shadow-[0_0px_5px_rgba(0,0,0,0.3)]"
-          style={{ left: `${2.8 * value - 30}px` }}
-        >
+        <label htmlFor="range" style={{ left: `${2.8 * value - 30}px` }}>
           {value}
         </label>
       </div>
