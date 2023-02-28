5e86afcec0d2c1159c8bbbad85d2ef45d8df8ebb
nick12003 <34929382+nick12003@users.noreply.github.com>
2023-02-28 13:51:27 +0800

    feat: 調整MobileNavigation

--- src/components/collections/MobileNavigation/index.jsx
+++ src/components/collections/MobileNavigation/index.jsx
@@ -8,70 +8,72 @@ const MobileNavigation = () => {
   const [active, setActive] = useState(false);
   return (
     <div className={styles.MobileNavigation}>
-      <button
-        className={classNames(styles['nav-btn'], styles['open-btn'])}
-        onClick={() => {
-          setActive(true);
-        }}
-      >
-        <FaBars />
-      </button>
+      <div className={styles.phone}>
+        <button
+          className={classNames(styles['nav-btn'], styles['open-btn'])}
+          onClick={() => {
+            setActive(true);
+          }}
+        >
+          <FaBars />
+        </button>
 
-      <img
-        src="https://www.logo.wine/a/logo/Netflix/Netflix-Logo.wine.svg"
-        alt="Logo"
-        className={styles.logo}
-      />
+        <img
+          src="https://www.logo.wine/a/logo/Netflix/Netflix-Logo.wine.svg"
+          alt="Logo"
+          className={styles.logo}
+        />
 
-      <p className={styles.text}>Mobile Navigation</p>
+        <p className={styles.text}>Mobile Navigation</p>
 
-      <div className={classNames(styles.nav, styles['nav-black'], { [styles.visible]: active })}>
-        <div className={classNames(styles.nav, styles['nav-red'], { [styles.visible]: active })}>
-          <div
-            className={classNames(styles.nav, styles['nav-white'], { [styles.visible]: active })}
-          >
-            <button
-              className={classNames(styles['nav-btn'], styles['close-btn'])}
-              onClick={() => {
-                setActive(false);
-              }}
+        <div className={classNames(styles.nav, styles['nav-black'], { [styles.visible]: active })}>
+          <div className={classNames(styles.nav, styles['nav-red'], { [styles.visible]: active })}>
+            <div
+              className={classNames(styles.nav, styles['nav-white'], { [styles.visible]: active })}
             >
-              <FaTimes />
-            </button>
+              <button
+                className={classNames(styles['nav-btn'], styles['close-btn'])}
+                onClick={() => {
+                  setActive(false);
+                }}
+              >
+                <FaTimes />
+              </button>
 
-            <img
-              src="https://www.logo.wine/a/logo/Netflix/Netflix-Logo.wine.svg"
-              alt="Logo"
-              className={styles.logo}
-            />
+              <img
+                src="https://www.logo.wine/a/logo/Netflix/Netflix-Logo.wine.svg"
+                alt="Logo"
+                className={styles.logo}
+              />
 
-            <ul className={styles.list}>
-              <li>
-                <a to="#">Teams</a>
-              </li>
-              <li>
-                <a to="#">Locations</a>
-              </li>
-              <li>
-                <a to="#">Life at Netflix</a>
-              </li>
-              <li>
-                <ul>
-                  <li>
-                    <a to="#">Netflix culture memo</a>
-                  </li>
-                  <li>
-                    <a to="#">Work life balance</a>
-                  </li>
-                  <li>
-                    <a to="#">Inclusion & diversity</a>
-                  </li>
-                  <li>
-                    <a to="#">Blog</a>
-                  </li>
-                </ul>
-              </li>
-            </ul>
+              <ul className={styles.list}>
+                <li>
+                  <a to="#">Teams</a>
+                </li>
+                <li>
+                  <a to="#">Locations</a>
+                </li>
+                <li>
+                  <a to="#">Life at Netflix</a>
+                </li>
+                <li>
+                  <ul>
+                    <li>
+                      <a to="#">Netflix culture memo</a>
+                    </li>
+                    <li>
+                      <a to="#">Work life balance</a>
+                    </li>
+                    <li>
+                      <a to="#">Inclusion & diversity</a>
+                    </li>
+                    <li>
+                      <a to="#">Blog</a>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
